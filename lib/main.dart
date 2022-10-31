f9b3c59be5e406acded849e3a9a63b2c22551eb3
aviacode <ilovemyroro624@gmail.com>
2022-10-31 23:17:53 +0900

    [SQUASH AND MERGE] UI: START MAKE BASE UI
    prev init
    pres continue
    next make map screen using leaflet.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,72 @@
 import "package:flutter/material.dart";
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Text("PROJECT-DIANA"),
-    ),
-  );
+  runApp(const Diana());
+}
+
+class Diana extends StatelessWidget {
+  const Diana({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: const ColorScheme.dark(),
+      ),
+      themeMode: ThemeMode.dark,
+      home: const MainFrame(),
+    );
+  }
+}
+
+class MainFrame extends StatelessWidget {
+  const MainFrame({super.key});
+
+  static AppBar _makeAppBar() {
+    return AppBar(
+      bottom: const TabBar(
+        tabs: [
+          Tab(text: "MapScreen"),
+          Tab(text: "EmptyTab"),
+        ],
+      ),
+    );
+  }
+
+  static Drawer _makeDrawer() {
+    return Drawer(
+      child: ListView(),
+    );
+  }
+
+  static TabBarView _makeTabBarView() {
+    return const TabBarView(
+      children: [
+        MapScreen(),
+        Center(child: Text("EmptyTab")),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: _makeAppBar(),
+        drawer: _makeDrawer(),
+        body: _makeTabBarView(),
+      ),
+    );
+  }
+}
+
+class MapScreen extends StatelessWidget {
+  const MapScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("MapScreen"));
+  }
 }
--- test/main_test.dart
+++ test/main_test.dart
@@ -2,5 +2,5 @@
 import "package:flutter_test/flutter_test.dart";
 
 void main() {
-  test("", () => {print("hello, world!")});
+  test("", () {});
 }
